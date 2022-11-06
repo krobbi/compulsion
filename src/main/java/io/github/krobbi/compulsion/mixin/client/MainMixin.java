e2d34a25fffdeba28e8100ab88846d2d2b398833
Chris Roberts <70552146+krobbi@users.noreply.github.com>
2022-11-06 15:11:04 +0000

    Mixin intrusiveness
    Make disabling data fixer less intrusive.

--- src/main/java/io/github/krobbi/compulsion/mixin/client/MainMixin.java
+++ src/main/java/io/github/krobbi/compulsion/mixin/client/MainMixin.java
@@ -2,14 +2,14 @@ package io.github.krobbi.compulsion.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import io.github.krobbi.compulsion.CompulsionMod;
 import net.minecraft.client.main.Main;
 
 @Mixin(Main.class)
 public abstract class MainMixin {
-	@ModifyVariable(method = "main([Ljava/lang/String;Z)V", at = @At("HEAD"), ordinal = 0)
+	@ModifyArg(method = "main([Ljava/lang/String;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/main/Main;main([Ljava/lang/String;Z)V"), index = 1)
 	private static boolean main(boolean optimizeDataFixer){
 		CompulsionMod.LOGGER.debug("Disabled data fixer optimization");
 		return false;
